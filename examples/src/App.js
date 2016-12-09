f2fc83586f25f30bf8728712f662228a384b5775
Martin Bielik <mx.bielik@gmail.com>
2016-12-09 16:31:15 +0100

    project structure

---
+++ examples/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View } from 'react-native'
+import { ColorPicker } from 'react-native-color-picker'
+
+export const App = () => (
+  <View>
+    <ColorPicker />
+  </View>
+)
+
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+export class ColorPicker extends Component {
+
+  render() {
+    return (
+      <View>
+        <Text>Hello world!</Text>
+      </View>
+    )
+  }
+
+}
