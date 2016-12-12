c18a196a4234d57c86f9ed323ce8ffd122f2080a
Martin Bielik <martin.bielik@outlook.sk>
2016-12-12 17:05:49 +0100

    vertical color picker

--- examples/src/App.js
+++ examples/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 import { ColorPicker } from 'react-native-color-picker'
 
 export const App = () => (
-  <View>
-    <ColorPicker />
+  <View style={{flex: 1, padding: 15, backgroundColor: '#212021'}}>
+    <Text style={{color: 'white'}}>React Native Color Picker</Text>
+    <ColorPicker originalColor='purple' onColorPicked={color => alert(`Color picked: ${color}`)} />
   </View>
 )
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,244 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import React, { Component, PropTypes } from 'react'
+import { PanResponder, TouchableOpacity, Slider, View, Image, StyleSheet } from 'react-native'
+import tinycolor from 'tinycolor2'
 
 export class ColorPicker extends Component {
 
+  constructor(props, ctx) {
+    super(props, ctx)
+    this.state = { width: 0, height: 0, H: 0, S: 100, V: 100 }
+    if (props.originalColor) {
+      const { h: H, s: S, v: V } = tinycolor(props.originalColor).toHsv()
+      Object.assign(this.state, { H, S, V })
+    }
+    this.onLayout = this.onLayout.bind(this)
+    this.onSValueChange = this.onSValueChange.bind(this)
+    this.onVValueChange = this.onVValueChange.bind(this)
+    this.onColorPicked = this.onColorPicked.bind(this)
+    this.onOriginalColorPressed = this.onOriginalColorPressed.bind(this)
+  }
+
+  onColorPicked() {
+    const { H, S, V } = this.state
+    const { onColorPicked } = this.props
+    const color = tinycolor({ h: H, s: S, v: V }).toRgbString()
+    onColorPicked && onColorPicked(color)
+  }
+
+  onOriginalColorPressed() {
+    const { originalColor } = this.props
+    const { h: H, s: S, v: V } = tinycolor(originalColor).toHsv()
+    this.setState({ H, S, V })
+  }
+
+  onSValueChange(S) {
+    this.setState({ S })
+  }
+
+  onVValueChange(V) {
+    this.setState({ V })
+  }
+
+  onLayout(l) {
+    const layout = l.nativeEvent.layout
+    if (['width', 'height'].some(key => layout[key] !== this.state[key])) {
+      const { width, height } = layout
+      this.setState({ width, height })
+    }
+  }
+
+  computeHValue(x, y) {
+    // TODO: make pickerSize at one place
+    const pickerSize = this.state.width
+    const mx = pickerSize / 2
+    const my = pickerSize / 2
+    const dx = x - mx
+    const dy = y - my
+    const rad = Math.atan2(dx, dy) + Math.PI + Math.PI / 2
+    return rad * 180 / Math.PI % 360
+  }
+
+  hValueToRad(deg) {
+    const rad = deg * Math.PI / 180
+    return rad - Math.PI - Math.PI / 2
+  }
+
+  componentWillMount() {
+    this._pickerResponder = createRelativePanResponder({
+      onStart: ({ x, y }) => {
+        this.setState({ H: this.computeHValue(x, y) })
+      },
+      onMove: ({ x, y }) => {
+        this.setState({ H: this.computeHValue(x, y) })
+      },
+    })
+  }
+
   render() {
+    const { width, height, H, S, V } = this.state
+    const { originalColor } = this.props
+    const angle = this.hValueToRad(H)
+    const hasDimensions = width && height
+    const selectedColor = tinycolor({ h: H, s: S, v: V }).toRgbString()
+    const indicatorColor = tinycolor({ h: H, s: 1, v: 1 }).toRgbString()
+    const s = makeComputedStyles({ width, height, selectedColor, indicatorColor, originalColor, angle })
+    // TODO: indicator is stealing touch events
     return (
-      <View>
-        <Text>Hello world!</Text>
+      <View style={styles.container} onLayout={this.onLayout}>
+        {!hasDimensions ? null :
+        <View>
+          <View style={[styles.pickerContainer, s.pickerContainer]} {...this._pickerResponder.panHandlers}>
+            <Image
+              source={require('../resources/color-circle.png')}
+              resizeMode='contain'
+              style={[styles.pickerImage]}
+            />
+            <View style={[styles.pickerIndicator, s.pickerIndicator]} />
+          </View>
+          <View style={styles.controls}>
+            <Slider value={S} onValueChange={this.onSValueChange} />
+            <Slider value={V} onValueChange={this.onVValueChange} />
+          </View>
+          {originalColor &&
+          <TouchableOpacity
+            style={[styles.selectedPreview, s.selectedPreview]}
+            onPress={this.onColorPicked}
+            activeOpacity={0.7}
+          />
+          }
+          {originalColor &&
+          <TouchableOpacity
+            style={[styles.originalPreview, s.originalPreview]}
+            onPress={this.onOriginalColorPressed}
+            activeOpacity={0.7}
+          />
+          }
+          {!originalColor &&
+          <TouchableOpacity
+            style={[styles.selectedFullPreview, s.selectedFullPreview]}
+            onPress={this.onColorPicked}
+            activeOpacity={0.7}
+          />
+          }
+        </View>
+        }
       </View>
     )
   }
 
+}
+
+ColorPicker.propTypes = {
+  originalColor: PropTypes.string,
+  onColorPicked: PropTypes.func,
+}
+
+const makeComputedStyles = ({
+  width,
+  indicatorColor,
+  selectedColor,
+  originalColor,
+  angle,
+}) => {
+  const pickerSize = width
+  const summarySize = 0.5 * pickerSize
+  const indicatorPickerRatio = 42 / 510 // computed from picker image
+  const indicatorSize = indicatorPickerRatio * pickerSize
+  const indicatorRadius = pickerSize / 2 - indicatorSize / 2
+  const mx = pickerSize / 2
+  const my = pickerSize / 2
+  const dx = Math.cos(angle) * indicatorRadius
+  const dy = Math.sin(angle) * indicatorRadius
+  return {
+    pickerContainer: {
+      width: pickerSize,
+      height: pickerSize,
+    },
+    pickerIndicator: {
+      top: mx + dx - indicatorSize / 2,
+      left: my + dy - indicatorSize / 2,
+      width: indicatorSize,
+      height: indicatorSize,
+      borderRadius: indicatorSize / 2,
+      backgroundColor: indicatorColor,
+    },
+    selectedPreview: {
+      width: summarySize / 2,
+      height: summarySize,
+      top: pickerSize / 2 - summarySize / 2,
+      left: pickerSize / 2,
+      borderTopRightRadius: summarySize / 2,
+      borderBottomRightRadius: summarySize / 2,
+      backgroundColor: selectedColor,
+    },
+    originalPreview: {
+      width: summarySize / 2,
+      height: summarySize,
+      top: pickerSize / 2 - summarySize / 2,
+      left: pickerSize / 2 - summarySize / 2,
+      borderTopLeftRadius: summarySize / 2,
+      borderBottomLeftRadius: summarySize / 2,
+      backgroundColor: originalColor,
+    },
+    selectedFullPreview: {
+      width: summarySize,
+      height: summarySize,
+      top: pickerSize / 2 - summarySize / 2,
+      left: pickerSize / 2 - summarySize / 2,
+      borderRadius: summarySize / 2,
+      backgroundColor: selectedColor,
+    },
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  pickerContainer: {
+  },
+  pickerImage: {
+    flex: 1,
+    width: null,
+    height: null,
+  },
+  pickerIndicator: {
+    position: 'absolute',
+    elevation: 5, // TODO: add shadow for ios
+  },
+  selectedPreview: {
+    position: 'absolute',
+    borderLeftWidth: 0,
+  },
+  originalPreview: {
+    position: 'absolute',
+    borderRightWidth: 0,
+  },
+  selectedFullPreview: {
+    position: 'absolute',
+  },
+})
+
+const fn = () => true;
+
+// responder which is passing relative x, y postion to parent view
+const createRelativePanResponder = ({ onStart = fn, onMove = fn, onEnd = fn }) => {
+  let marginX = 0
+  let marginY = 0
+  return PanResponder.create({
+    onStartShouldSetPanResponder: fn,
+    onStartShouldSetPanResponderCapture: fn,
+    onPanResponderTerminationRequest: fn,
+    onPanResponderGrant: (evt, state) => {
+      marginX = state.x0 - evt.nativeEvent.locationX
+      marginY = state.y0 - evt.nativeEvent.locationY
+      return onStart({ x: state.x0 - marginX, y: state.y0 - marginY }, evt, state)
+    },
+    onPanResponderMove: (evt, state) => {
+      return onMove({ x: state.moveX - marginX, y: state.moveY - marginY }, evt, state)
+    },
+    onPanResponderRelease: (evt, state) => {
+      return onEnd({ x: state.moveX - marginX, y: state.moveY - marginY }, evt, state)
+    },
+  })
 }
