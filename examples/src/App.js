c8604f1e572855a8faa2ce9879c10f0ebd6e071f
Martin Bielik <mx.bielik@gmail.com>
2016-12-14 11:52:22 +0100

    vertical picker implementation
    commit 5d5e3d595f2975620763a21cde08d6ac5c9d9f88
    Author: Martin Bielik <mx.bielik@gmail.com>
    Date:   Wed Dec 14 11:33:08 2016 +0100
    
        adaptive layouts, passing styles
    
    commit 387d9069d6424a0b1c76be45ce318d8b0007027a
    Author: Martin Bielik <mx.bielik@gmail.com>
    Date:   Tue Dec 13 17:30:13 2016 +0100
    
        adaptive layout
    
    commit b1892be63d456deeb80ec45e361e701ecda95d43
    Author: Martin Bielik <mx.bielik@gmail.com>
    Date:   Tue Dec 13 14:17:16 2016 +0100
    
        picker padding
    
    commit aea83da41e1ebf64e4d86e5a55671d352dfe45cb
    Author: Martin Bielik <mx.bielik@gmail.com>
    Date:   Tue Dec 13 13:47:17 2016 +0100
    
        public/privat api
    
    commit e11f9b7ce2ee4c889b3079abf83ed9cb49d9746e
    Author: Martin Bielik <mx.bielik@gmail.com>
    Date:   Tue Dec 13 13:27:45 2016 +0100
    
        controlled api
    
    commit c20e49483bdbc235393560e318550ef766fe0b26
    Author: Stanislav Miklik <stanislav.miklik@gmail.com>
    Date:   Tue Dec 13 10:58:57 2016 +0100
    
        add ios shaddows
    
    commit 8ebe861ac81ec5cf35667da5a12d5fdf20390408
    Author: Stanislav Miklik <stanislav.miklik@gmail.com>
    Date:   Tue Dec 13 10:11:09 2016 +0100
    
        update examples to fixed version of RN
    
    commit 67ca7fa07666a2ea816c3cf21638e787ff5f25e4
    Author: Martin Bielik <martin.bielik@outlook.sk>
    Date:   Mon Dec 12 17:52:07 2016 +0100
    
        measure picker position in screen
    
    commit c18a196a4234d57c86f9ed323ce8ffd122f2080a
    Author: Martin Bielik <martin.bielik@outlook.sk>
    Date:   Mon Dec 12 17:05:49 2016 +0100
    
        vertical color picker

--- examples/src/App.js
+++ examples/src/App.js
@@ -1,10 +1,57 @@
 import React from 'react'
-import { View } from 'react-native'
-import { ColorPicker } from 'react-native-color-picker'
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
+import { ExampleUncontrolledVertical } from './ExampleUncontrolledVertical'
+import { ExampleControlledVertical } from './ExampleControlledVertical'
 
-export const App = () => (
-  <View>
-    <ColorPicker />
-  </View>
-)
+const examples = [
+  { Component: ExampleUncontrolledVertical, title: 'Uncontrolled vertical picker' },
+  { Component: ExampleControlledVertical, title: 'Controlled vertical picker' },
+]
 
+export class App extends React.Component {
+
+  constructor(...args) {
+    super(...args)
+    this.state = { example: null }
+  }
+
+  onColorChange(color) {
+    this.setState({ color })
+  }
+
+  render() {
+    const { example } = this.state
+    if (example) {
+      const { Component } = example
+      return <Component />
+    }
+    return (
+      <View style={styles.container}>
+        {examples.map(example => (
+          <TouchableOpacity
+            key={example.title}
+            style={styles.touchable}
+            onPress={() => this.setState({ example })}
+          >
+            <Text style={styles.text}>{example.title}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+    )
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 15,
+    backgroundColor: '#212021',
+  },
+  touchable: {
+    padding: 5,
+  },
+  text: {
+    color: 'white',
+  },
+})
---
+++ examples/src/ExampleControlledVertical.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { ColorPicker, toHsv } from 'react-native-color-picker'
+
+export class ExampleControlledVertical extends React.Component {
+
+  constructor(...args) {
+    super(...args)
+    this.state = { color: toHsv('green') }
+    this.onColorChange = this.onColorChange.bind(this)
+  }
+
+  onColorChange(color) {
+    this.setState({ color })
+  }
+
+  render() {
+    return (
+      <View style={{flex: 1, padding: 15, backgroundColor: '#212021'}}>
+        <Text style={{color: 'white'}}>React Native Color Picker - Controlled</Text>
+        <ColorPicker
+          oldColor='purple'
+          color={this.state.color}
+          onColorChange={this.onColorChange}
+          onColorSelected={color => alert(`Color selected: ${color}`)}
+          onOldColorSelected={color => alert(`Old color selected: ${color}`)}
+          style={{flex: 1}}
+        />
+      </View>
+    )
+  }
+
+}
+
